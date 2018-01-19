f9e794665de98073fd7bd3f2f52e3d06703c7fbc
Bob Carroll <avpbob@gmail.com>
2018-01-19 13:52:11 -0500

    friends.js file updated need to fix how pics display

---
+++ app/data/friends.js
@@ -0,0 +1,105 @@
+// DATA
+// friends array to hold all of the possible friends.
+
+var friends = [
+  {
+    name: "Bill Adams",
+    photo: "https://pixabay.com/en/portrait-facial-hair-people-3052641/",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Goldie Graham",
+    photo: "https://cdn.pixabay.com/photo/2018/01/14/18/53/woman-3082371_960_720.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Lawrence Wills",
+    photo: "https://cdn.pixabay.com/photo/2018/01/16/23/24/portrait-3087130_960_720.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Shirley Long",
+    photo: "https://cdn.pixabay.com/photo/2018/01/12/21/06/excited-3079073_960_720.jpg",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Steve Silver",
+    photo: "https://cdn.pixabay.com/photo/2018/01/04/08/04/portrait-3060106_960_720.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Tom Canon",
+    photo: "https://cdn.pixabay.com/photo/2018/01/17/15/21/one-3088442_960_720.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
